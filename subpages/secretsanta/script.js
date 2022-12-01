0f9fd89f785caa7fcd84b23a3c0bb52cdffc1c66
Alec Grover <39810460+AlecGrover@users.noreply.github.com>
2022-12-01 11:10:40 -0800

    Update script.js

--- subpages/secretsanta/script.js
+++ subpages/secretsanta/script.js
@@ -91,7 +91,7 @@ function get_target() {
 }
 
 let gList= new Array()
-let gCoded= ""
+let gCoded= "\\u005bĀĂ22Ć041Ċ6cĎ5Ď3ĊĈĕĐā0Ėę4aĎfĊ7Ĕę68ĕĉę2ĘćĨĂ4ąĤĒĤī0ďġ9ħėĹĜdĎčę7ĭĵģĂ7ĲŅńĚłĪĻĂ5ŉ6ĿĂ6Ľĩł5d"
 let gSeed= "Christmas"
 
 const configbutton= document.getElementById("configbutton");
