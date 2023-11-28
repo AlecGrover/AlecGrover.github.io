1ecc1ed4f815100af37aa738fd700f17ec5b9822
Alec Grover <39810460+AlecGrover@users.noreply.github.com>
2023-11-28 13:48:00 -0800

    Update script.js

--- subpages/secretsanta/script.js
+++ subpages/secretsanta/script.js
@@ -95,7 +95,7 @@ function get_target() {
 
 let gList= new Array();
 let gCoded= "\\u005bĀĂ22Ć041Ċ6cĎ5Ď3ĊĈĕĐā0Ėę4aĎfĊ7Ĕę68ĕĉę2ĘćĨĂ4ąĤĒĤī0ďġ9ħėĹĜdĎčę7ĭĵģĂ7ĲŅńĚłĪĻĂ5ŉ6ĿĂ6Ľĩł5d";
-let gSeed= "Carolling Nightmare Before Krampus";
+let gSeed= "Carolling Nightmare before Krampus";
 
 const configbutton= document.getElementById("configbutton");
 configbutton.addEventListener("click", configure)
